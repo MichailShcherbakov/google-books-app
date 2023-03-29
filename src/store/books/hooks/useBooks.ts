debadfece29751ec74e99eb924d5ffcf38b90d8f
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2023-03-29 23:14:18 +0300

    fix: not correct book criteria with browser search params

--- src/store/books/hooks/useBooks.ts
+++ src/store/books/hooks/useBooks.ts
@@ -1,25 +1,11 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
 import { useAppSelector } from "~/store/hooks";
-import { CategoryFilterEnum, RequestStatusEnum, SortByEnum } from "../type";
-import { useCurrentBookSearchFilterBy } from "./useCurrentBookSearchFilterBy";
-import { useCurrentBookSearchPattern } from "./useCurrentBookSearchPattern";
-import { useCurrentBookSearchSortBy } from "./useCurrentBookSearchSortBy";
-import { useRequestBooks } from "./useRequestBooks";
+import { RequestStatusEnum } from "../type";
+import { useBookSearchParamSync } from "./useBookSearchParamSync";
 
 export function useBooks() {
-  const isInitialized = React.useRef(false);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  const setSearchParamsStable =
-    React.useRef<
-      (params: Record<string, string>, options?: { replace?: boolean }) => void
-    >(setSearchParams);
-
-  setSearchParamsStable.current = setSearchParams;
-
-  const criteria = useAppSelector(state => state.books.criteria);
   const books = useAppSelector(state => state.books.all);
+
+  useBookSearchParamSync();
 
   const isLoading = useAppSelector(
     state => state.books.status === RequestStatusEnum.REQUESTED,
@@ -30,51 +16,6 @@ export function useBooks() {
   );
 
   const isEmpty = useAppSelector(state => !state.books.all.length);
-
-  const { requestBooks } = useRequestBooks();
-
-  const { setPattern } = useCurrentBookSearchPattern();
-  const { setFilterBy } = useCurrentBookSearchFilterBy();
-  const { setSortBy } = useCurrentBookSearchSortBy();
-
-  React.useEffect(() => {
-    if (isInitialized.current) return;
-
-    const pattern = searchParams.get("q") ?? "";
-    let filterBy = searchParams.get("filterBy") as CategoryFilterEnum;
-    let sortBy = searchParams.get("sortBy") as SortByEnum;
-
-    filterBy = Object.values(CategoryFilterEnum).includes(filterBy)
-      ? filterBy
-      : CategoryFilterEnum.ALL;
-
-    sortBy = Object.values(SortByEnum).includes(sortBy)
-      ? sortBy
-      : SortByEnum.RELEVANCE;
-
-    setPattern(pattern);
-    setFilterBy(filterBy);
-    setSortBy(sortBy);
-  }, [requestBooks, searchParams, setFilterBy, setPattern, setSortBy]);
-
-  React.useEffect(() => {
-    // skip initial criteria changing
-    if (!isInitialized.current) {
-      isInitialized.current = true;
-      return;
-    }
-
-    setSearchParamsStable.current(
-      {
-        q: criteria.pattern,
-        filterBy: criteria.filterBy,
-        sortBy: criteria.sortBy,
-      },
-      { replace: true },
-    );
-
-    requestBooks();
-  }, [requestBooks, criteria.pattern, criteria.filterBy, criteria.sortBy]);
 
   return {
     books,
